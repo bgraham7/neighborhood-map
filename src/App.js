3d78a46c47ec76e74350d77d64c24e79bb2e136f
bgraham <bgraham626@gmail.com>
2018-11-07 18:52:15 -0500

    Using Times Square Default Coords

--- src/App.js
+++ src/App.js
@@ -6,11 +6,17 @@ import * as fourSquare from "./apis/foursquare-api";
 
 class App extends Component {
 
+  // Time Square
+  myCoords = {
+    lat: 40.758896,
+    long: -73.985130
+  }
+
   map;
 
   renderMap = () => {
     loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyDCmIPpcyP0wo2rE9LPDUYtFCHqapw2TIQ&callback=initMap");
-    fourSquare.searchPlaces(0, 0, "")
+    fourSquare.searchPlaces(this.myCoords.lat, this.myCoords.long)
     .then(function(data) {
       console.log(data);
     })
@@ -22,11 +28,12 @@ class App extends Component {
   componentDidMount() {
     this.renderMap();
     window.initMap = this.initMap;
+    window.myCoords = this.myCoords;
   }
 
   initMap() {
     this.map = new window.google.maps.Map(document.getElementById('map'), {
-      center: {lat: -34.397, lng: 150.644},
+      center: {lat: this.myCoords.lat, lng: this.myCoords.long},
       zoom: 8
     });
   }
