c0e0cb96dd49f3b8bf0ccce2a83505106bf641c8
bgraham <bgraham626@gmail.com>
2018-11-07 19:30:22 -0500

    Switching to google-react-maps

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import loadScript from "./functions/loadScript";
+import SimpleMap from './components/map';
 import * as fourSquare from "./apis/foursquare-api";
 
 class App extends Component {
@@ -9,39 +9,40 @@ class App extends Component {
   // Time Square
   myCoords = {
     lat: 40.758896,
-    long: -73.985130
+    lng: -73.985130
   }
 
-  map;
+  markers;
 
-  renderMap = () => {
-    loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyDCmIPpcyP0wo2rE9LPDUYtFCHqapw2TIQ&callback=initMap");
-    fourSquare.searchPlaces(this.myCoords.lat, this.myCoords.long)
+  componentDidMount() {
+    
+  }
+
+
+  searchPlaces() {
+    fourSquare.searchPlaces(this.myCoords.lat, this.myCoords.lng)
     .then(function(data) {
-      console.log(data);
+      var items = data.response.groups[0].items;
+      console.log(items);
+      for(let i = 0; i < items.length; i++) {
+        let item = items[i];
+        var marker = new window.google.maps.Marker({
+          position: { lat: item.venue.location.lat, lng: item.venue.location.lng },
+          map: this.map,
+          title: item.venue.name
+        });
+        this.markers.push(marker);
+      }
     })
     .catch(function(err) {
         console.log(err);
     });
   }
 
-  componentDidMount() {
-    this.renderMap();
-    window.initMap = this.initMap;
-    window.myCoords = this.myCoords;
-  }
-
-  initMap() {
-    this.map = new window.google.maps.Map(document.getElementById('map'), {
-      center: {lat: this.myCoords.lat, lng: this.myCoords.long},
-      zoom: 8
-    });
-  }
-
   render() {
     return (
-      <div className="App">
-        <div id="map"></div>
+      <div className="map-wrapper">
+        <SimpleMap />
       </div>
     );
   }
