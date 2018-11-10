834bd1d78bcae1c4fc3e34a300f86ea1351e0fa0
bgraham <bgraham626@gmail.com>
2018-11-09 19:59:03 -0500

    Responsive Search Bar

--- src/components/MarkerContentBar.js
+++ src/components/MarkerContentBar.js
@@ -3,6 +3,13 @@ import './MarkerContentBar.css';
 
 class App extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+          menuOpen: false
+        };
+      }
+
     // https://medium.com/@griffinmichl/implementing-debounce-in-javascript-eab51a12311e
     debounce(func, wait) {
         let timeout
@@ -19,24 +26,41 @@ class App extends Component {
         }, 200);
     }
 
+    toggleMenu() {
+        this.setState({
+            menuOpen: !this.state.menuOpen
+        })
+    }
+
   render() {
     return (
-      <div className="content-bar">
-          <h1>NY Places</h1>
-          <div className="search-bar">
-            <input 
-                onKeyUp={(e)=>this.searchPlaces(e.target.value)}
-                className="search-box" type="text" />
-            <i className="fas fa-search search-icon"></i>
+      <section className="content-bar">
+          <header class="header">
+            <div class="hamburger" onClick={() => this.toggleMenu()}>
+                <div class="bar"></div>
+                <div class="bar"></div>
+                <div class="bar"></div>
+            </div>
+            <h1>NY Places</h1>
+          </header>
+          
+          <div className={'place-filter-pane ' + (this.state.menuOpen? 'open' : '')} >
+            <div className="search-bar">
+                <input 
+                    onKeyUp={(e)=>this.searchPlaces(e.target.value)}
+                    className="search-box" type="text" />
+                <i className="fas fa-search search-icon"></i>
+            </div>
+            <div className="places">
+                {this.props.markers.map(marker => (
+                    <div className={`place ${marker.isOpen? 'open': ''}`} 
+                    onClick={() => this.props.toggleInfoBox(marker.id)}
+                    key={marker.id}>{marker.title}</div>
+                ))}
+            </div>
           </div>
-          <div className="places">
-            {this.props.markers.map(marker => (
-                <div className={`place ${marker.isOpen? 'open': ''}`} 
-                onClick={() => this.props.toggleInfoBox(marker.id)}
-                key={marker.id}>{marker.title}</div>
-            ))}
-          </div>
-      </div>
+          
+      </section>
     );
   }
 }
