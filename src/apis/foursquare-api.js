7813cc42218d092aa805b0be3ff079f66e8599f7
bgraham <bgraham626@gmail.com>
2018-11-11 11:16:37 -0500

    Customer Google Maps Error Handling

--- src/apis/foursquare-api.js
+++ src/apis/foursquare-api.js
@@ -1,4 +1,4 @@
-export function searchPlaces(lat, long, query = "pizza") {
+export function searchPlaces(lat, long, query) {
     const baseUrl = "https://api.foursquare.com/v2/venues/search?client_id=ODZW5JKF2DDGFLXRTR1R4XQ5MODMUB0BQBMI3DJZXLAUVLAB&client_secret=QBI4ZTWXEO4ZGNAU4GTGLOJGETJMUSQBESUKXJCG04IUOP4A&v=20180323&limit=10";
     let url = `${baseUrl}&ll=${lat},${long}`
     if(query) {
